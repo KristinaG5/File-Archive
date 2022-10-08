713faaf6ab6d7ef4263dde804ff55bcfb83ccdc6
Kristina <kgeddis950@gmail.com>
2022-10-08 13:57:35 +0200

    Create fetch and upload endpoints

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
+import os
+import enum
+from pkgutil import extend_path
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-import enum
-import os
+from datetime import date
 
 
 app=Flask(__name__)
 load_dotenv()
+UPLOAD_FOLDER = "uploads"
 
 # Database
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_PATH')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 
 #Enum
@@ -25,11 +29,41 @@ class File(db.Model):
     type = db.Column(db.Enum(FileType), nullable=False)
     filename = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(80), nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    date = db.Column(db.Date, default=date.today)
 
-# Setup
-with app.app_context():
-    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+def get_nonconflicting_filename(filename: str):
+    name, extension = filename.split(".")
+    i = 1
+    while filename in os.listdir(UPLOAD_FOLDER):
+        filename = f"{name}-{i}.{extension}"
+        i+=1
+    return filename
+
+
+@app.route("/", methods=['POST'])
+def upload():
+    upload = request.files["file"] 
+    type = upload.mimetype.split("/")[1]
+    filename = get_nonconflicting_filename(upload.filename)
+    
+    file = File(type=type, filename=filename, description="")
+    db.session.add(file)
+    db.session.commit()
+
+    upload.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
